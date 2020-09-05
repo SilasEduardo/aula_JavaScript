6867420514c43e6af3fea1a233fa02f36a887cbc
Silas Eduardo de Paula <silpau@intra.olos.com.br>
2020-09-05 00:22:02 -0300

    novo exercicios

--- aulas-fullstack-IGTI/aula-06/js/scripts.js
+++ aulas-fullstack-IGTI/aula-06/js/scripts.js
@@ -1,3 +1,30 @@
-const nome = "silas";
+window.addEventListener('load', start);//carregando apagina
 
-console.log(nome);
+
+function start(){
+    console.log('aula 04');
+    console.log('pagina carregada')
+
+    const span = document.querySelector('#nameLength'); // elecionando o ID do elemento html
+    var nameImput = document.querySelector('#nameImput');//delecuonando o imput HTML
+    nameImput.addEventListener('keyup', countName);
+
+    var form = document.querySelector('form');
+    form.addEventListener('submint', preventSubmint);
+}
+
+
+function countName(vent){
+    var conut = event.target.value
+
+    const span = document.querySelector('#nameLength'); // elecionando o ID do elemento html
+    span.textContent = conut.length;
+  
+}
+function preventSubmint(event){
+event.preventDefault();
+
+var nameImput = document.querySelector('#nameImput');
+alert(nameImput.value + 'cadastrado com sucesso');
+
+}
